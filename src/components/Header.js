46a1500077b8f405466a0243a8eef8ebcf317e3e
Weybee Solutions <96857532+CharmiWeybee@users.noreply.github.com>
2022-07-15 18:05:10 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+
+export const Header = () => {
+    return (
+        <>
+            <div className='header d-flex'>
+                <div className='nav-left d-flex'>
+                    <div className='nav-logo'>
+                        logo
+                    </div>
+                    <div className='nav-search'>
+                        search
+                    </div>
+                    <div className='nav-right'>
+                        cart
+                    </div>
+
+                </div>
+            </div>
+        </>
+    )
+}
